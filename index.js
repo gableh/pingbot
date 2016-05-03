72e47f8bbb524fffe47d6ab891da7f054565f69e
gableh <gable.heng@gmail.com>
2016-05-04 00:33:11 +0800

    can now check if websites are up or down

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 
 module.exports = function(robot) {
-  robot.respond(/hello world/, function(msg, done) {
-    msg.reply("hello back", done);
+
+  robot.respond(/ping( )?(.*)?/i, function(msg, done) {
+    website = msg.match[2];
+    if(website === undefined){
+      msg.reply("Please type in a website, e.g ping www.asknestor.me", done);
+    } else {
+      if(website.search(/http.*/) == -1){
+        formattedWebsite = "https://" + website;
+      }
+      robot.http(formattedWebsite).head()(function(err, res) {
+        if(res === null || res.statusCode != "200"){
+          msg.reply(website + " is down! :slightly_frowning_face:",done);
+        } else {
+          msg.reply(website + " is up! :simple_smile:",done);
+        }
+      });
+    }
   });
 };
